4a096125597c2f240a08b2f4e339a31466f8da3d
TwitchJP <week-twitchy.0s@icloud.com>
2025-05-13 15:18:24 +0530

    reset

--- _site/assets/javascripts/lunr/lunr.store.js
+++
@@ -1,6 +0,0 @@
-var store = [{
-        "title": "Unix commands every developer should know",
-        "excerpt":"If you are new to Linux or any Unix operating system here is a list of must know commands that can help you in every day task cat Shows the content of a file. Run cat /etc/hosts 127.0.0.1 localhost255.255.255.255 broadcasthost::1 localhostis the output on my computer. /etc/hosts is the path...","categories": ["programming"],
-        "tags": ["unix","command-line"],
-        "url": "http://localhost:4000/programming/2021/07/22/unix-commands-every-developer-should-know.html"
-      }]
--- about.md
+++
@@ -1,8 +0,0 @@
----
-title: "About"
-layout: about
----
-
-I am Ankur Gupta. Live in Bengaluru, India with my wife and a son. Software Development is both a hobby and profession. 
-I actively code in Python, Go, Dart. 
-Have worked for 15 years professionally at few VC funded startups and a then Fortune 10 company.
